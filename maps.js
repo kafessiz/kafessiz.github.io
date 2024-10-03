413964cc83166e16ae34bfa9c12755ec8e94cfd6
kafessiz <aksiyon@kafessizturkiye.com>
2024-10-03 17:00:08 +0300

    Create maps.js

---
+++ maps.js
@@ -0,0 +1,20 @@
+const addresses = [
+    "https://maps.app.goo.gl/PC89zWtsSw6G7vLY7",
+    "https://maps.app.goo.gl/z42XSS71jPFo7WsT6",
+    "https://maps.app.goo.gl/XSRkQnDnwfQR2ySK7",
+    "https://maps.app.goo.gl/caGU6dBmVzPnqAGy5",
+    "https://maps.app.goo.gl/RhVgaEPMeEWqKMka9",
+    "https://maps.app.goo.gl/fmC5hpUCovVstdwJ8",
+    "https://maps.app.goo.gl/CRioHLHfm8Eg49ah9",
+    "https://maps.app.goo.gl/EQkdPFY28qexhQi47",
+    "https://maps.app.goo.gl/Qg8i7ooUMYWPQFXk7",
+    "https://maps.app.goo.gl/8jeoM6MNZXiKZ7wr8",
+    "https://maps.app.goo.gl/S6Sa7fpGxgdyfDU59",
+  
+    
+
+];
+
+const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
+
+window.location.replace(address_1);
