86e51bc753a45319c465bf16f792e420c56da50a
kafessiz <aksiyon@kafessizturkiye.com>
2025-01-08 18:51:50 +0300

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,18 +1,13 @@
 const addresses = [
-    "https://maps.app.goo.gl/71gPBw6TeYgTrDaL6",
-    "https://maps.app.goo.gl/QxKChsotmM3e5FF78",
-    "https://maps.app.goo.gl/kcftg5XQ7dBT2e5h9",
-    "https://maps.app.goo.gl/iz4V8SXC2Qm1FaC88",
-    "https://maps.app.goo.gl/cy98uLBsSFu44Ji19",
-    "https://maps.app.goo.gl/uS79kipvgWAkDqti9",
-    "https://maps.app.goo.gl/2B6uVSfMkoXbMwQY6",
-    "https://maps.app.goo.gl/FLnkWDm8zqa2xHEFA",
-    "https://maps.app.goo.gl/JrQh2iLfJoZXyUfC7",
+    "https://maps.app.goo.gl/cViP5HHsUD1R7Nv48",
+    "https://maps.app.goo.gl/iThsKAJezc7YLA399",
     "https://maps.app.goo.gl/FCRY9ZzikU2hRXkSA",
     "https://maps.app.goo.gl/duFqF82jakr1Q3dE9",
     "https://maps.app.goo.gl/x4WJWiNEos4u58XM8",
     "https://maps.app.goo.gl/wBxL758ahUDWcykV7",
-    "https://maps.app.goo.gl/4bqcq1H1eN9q1b6g9"
+    "https://maps.app.goo.gl/4bqcq1H1eN9q1b6g9",
+    "https://maps.app.goo.gl/YUTsFY1ybB5FGtd99",
+    "https://maps.app.goo.gl/773yAKxNnaPdcPSj6"
     
   
     
