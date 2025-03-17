8687e4c3b23d6908a93a73d533faa0f3a6d22997
kafessiz <aksiyon@kafessizturkiye.com>
2025-03-17 18:03:34 +0300

    Update maps.js

--- maps.js
+++ maps.js
@@ -6,7 +6,11 @@ const addresses = [
     "https://maps.app.goo.gl/773yAKxNnaPdcPSj6",
     "https://maps.app.goo.gl/DqFDoe7qrBMGKXCdA",
     "https://maps.app.goo.gl/wQ2K3rvyCr2o7TAL7",
-    "https://maps.app.goo.gl/7HUHEj8rEdJPSaaA9"
+    "https://maps.app.goo.gl/7HUHEj8rEdJPSaaA9",
+    "https://maps.app.goo.gl/YE23Rj73vLmtvxve9",
+    "https://maps.app.goo.gl/2TcWoVfhxrq1Wwcd8",
+    "https://maps.app.goo.gl/nDPzzvgXotz9t8rQ9",
+    "https://maps.app.goo.gl/3Hz2x7KMzX4x91t68"
     
   
     
